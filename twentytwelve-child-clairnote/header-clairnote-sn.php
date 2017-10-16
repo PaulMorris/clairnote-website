658e0e158461f791fd33eed44966990869be7411
Paul Morris <paul@paulwmorris.com>
2017-10-16 00:43:42 -0400

    Fix text and link of mobile screen site title for Clairnote SN

--- twentytwelve-child-clairnote/header-clairnote-sn.php
+++ twentytwelve-child-clairnote/header-clairnote-sn.php
@@ -36,7 +36,7 @@
 <div id="page" class="hfeed site">
     <header id="masthead" class="site-header" role="banner">
         <hgroup>
-            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            <h1 class="site-title"><a href="http://clairnote.org/sn/" title="Clairnote SN Music Notation" rel="home">Clairnote SN Music Notation</a></h1>
             <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
         </hgroup>
 
