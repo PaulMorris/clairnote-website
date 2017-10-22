a4863de28d8cb33fcbc64c26ec8cd13e05bf21a8
Paul Morris <paul@paulwmorris.com>
2017-10-22 11:31:59 -0400

    Let clairnote-sn-switch.js modify an item in first/home menu
    The `More` menu is now gone and its contents are under the first
    menu item (home / `Clairnote Music Notation`).  So we edit the JS
    for dynamically modifying the menu to allow switching between
    Clairnote and Clairnote SN to fit the new menu structure.
    
    Rather than `Switch to Clairnote SN site` or similar, we use
    `Clairnote SN - Current Page` in parallel to `Clairnote - Home`.

--- twentytwelve-child-clairnote/js/clairnote-sn-switch.js
+++ twentytwelve-child-clairnote/js/clairnote-sn-switch.js
@@ -7,37 +7,19 @@
     // Just for good measure. This JS should not even be loaded on blog pages.
     if (path[0] === 'blog') { return; }
 
-    var isSN = path[0] === 'sn';
-    var toPath = isSN ? path.slice(1) : Array.concat(['sn'], path);
+    var isSN = path[0] === 'sn',
+        toPath = isSN ? path.slice(1) : Array.concat(['sn'], path),
+        isHome = isSN ? path.length === 1 : path.length === 0,
 
-    var a = document.createElement('a');
-    a.href = "http://clairnote.org/" + toPath.join('/');
+        outer_ul = document.getElementById(isSN ? 'menu-clairnote-sn' : 'menu-main-menu'),
+        outer_ul_li = outer_ul.firstElementChild,
 
-    a.appendChild(document.createTextNode(
-        'Switch to Clairnote ' + (isSN ? '' : 'SN ') + 'site'
-    ));
+        ul = outer_ul_li.querySelector('.sub-menu'),
+        ul_lis = document.querySelectorAll('li'),
+        ul_li = ul_lis[2],
+        ul_li_a = ul_li.firstElementChild,
+        modifier = isHome ? ' — Home' : ' — Current Page';
 
-    var ul = document.getElementById(
-        isSN ? 'menu-clairnote-sn' : 'menu-main-menu'
-    );
-
-    var ul_li = ul.lastElementChild;
-    ul_li.classList.add(
-        'menu-item-type-custom',
-        'menu-item-object-custom',
-        'menu-item-has-children'
-    );
-
-    var ul_li_ul = ul_li.querySelector('.sub-menu');
-    var ul_li_ul_li = document.createElement('li');
-
-    ul_li_ul_li.classList.add(
-        'menu-item',
-        'menu-item-type-custom',
-        'menu-item-object-custom'
-    );
-
-    ul_li_ul_li.appendChild(a);
-    ul_li_ul.appendChild(ul_li_ul_li);
-    ul_li.appendChild(ul_li_ul);
+    ul_li_a.href = 'http://clairnote.org/' + toPath.join('/');
+    ul_li_a.textContent = 'Clairnote' + (isSN ? '' : ' SN') + modifier;
 })();
